2a2c3114cc979ecc1ce505caed92843bdb3d8b8e
ykzZa <n.hryshaienko@gmail.com>
2023-05-11 21:12:20 +0300

    lab2

--- lab2/services/fruit-service/server.js
+++ lab2/services/fruit-service/server.js
@@ -2,68 +2,135 @@
 
 const express = require('express')
 const bodyParser = require('body-parser');
+const { Pool } = require('pg');
 var cors = require('cors')
 const PORT = 8080
 const HOST = '0.0.0.0'
 
-let fruits = [
-  {id: 1, name: 'Banana', count: 10},
-  {id: 2, name: 'Apple', count: 7},
-  {id: 3, name: 'Kiwi', count: 25 }
+let orders = [
+  {id: 1, veggie: 'Carrots', count: 2, address: 'Kyiv'},
+  {id: 2, veggie: 'Broccoli', count: 4, address: 'Lviv'},
+  {id: 3, veggie: 'Eggplant', count: 1, address: 'Dnipro' }
 ];
 
 const app = express()
 app.use(bodyParser.json());
 app.use(cors());
-app.get('/service1', (req, res) => {
-  res.send('boje pomoji')
+
+const pool = new Pool({
+  user: "demo",
+  host: "postgres-orders",
+  database: "demo",
+  password: "demo",
+  port: 5432
 })
 
+
+pool.connect()
+  .then(() => {
+    return pool.query();
+    })
+    .then((res) => {
+      console.log(res)
+      console.log('Successfully connected to database and ensured table exists');
+    })
+    .catch((err) => {
+      console.error('Error connecting to database:', err);
+    });
+
 // READ operation
-app.get('/fruits/:id', (req, res) => {
-  const fruit = fruits.find(f => f.id === parseInt(req.params.id));
-  if (!fruit) {
-    res.status(404).send('Fruit not found');
-  } else {
-    res.send(fruit);
+app.get('/orders/:id', async (req, res) => {
+  try {
+    const query = {
+      text: 'SELECT * FROM orders WHERE id = $1',
+      values: [req.params.id],
+    };
+
+    const result = await pool.query(query);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Order not found' });
+    }
+
+    return res.json(result.rows[0]);
+  } catch (err) {
+    console.error('Error retrieving order:', err);
+    return res.status(500).json({ error: 'Failed to retrieve order' });
   }
 });
 
 //ALL fruits
-app.get('/fruits', (req, res) => {
-  res.send(fruits);
+app.get('/orders', async (req, res) => {
+  try {
+    const query = {
+      text: 'SELECT * FROM orders',
+    };
+
+    const result = await pool.query(query);
+    res.json(result.rows);
+  } catch (err) {
+    console.error('Error retrieving orders:', err);
+    res.status(500).json({ error: 'Failed to retrieve orders' });
+  }
 });
 
 //CREATE operation
-app.post('/fruits', (req, res) => {
-  const fruit = req.body;
-  fruit.id = fruits.length + 1;
-  fruits.push(fruit);
-  res.send(fruit);
+app.post('/orders', async (req, res) => {
+  try {
+    const { veggie, count, address } = req.body;
+    const query = {
+      text: 'INSERT INTO orders(veggie, count, address) VALUES($1, $2, $3) RETURNING *',
+      values: [veggie, count, address],
+    };
+
+    const result = await pool.query(query);
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error('Error adding order:', err);
+    res.status(500).json({ error: 'Failed to add order' });
+  }
 });
 
 // UPDATE operation
-app.put('/fruits/:id', (req, res) => {
-  const fruit = fruits.find(f => f.id === parseInt(req.params.id));
-  if (!fruit) {
-    res.status(404).send('Fruit not found');
-  } else {
-    fruit.name = req.body.name || fruit.name;
-    fruit.count = req.body.count || fruit.count;
+app.put('/orders/:id', async (req, res) => {
+  try {
+    const { veggie, count, address } = req.body;
+    const query = {
+      text: 'UPDATE orders SET veggie = $1, count = $2, address = $3 WHERE id = $4 RETURNING *',
+      values: [veggie, count, address, req.params.id],
+    };
 
-    res.send('Fruit updated successfully');
+    const result = await pool.query(query);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Order not found' });
+    }
+
+    return res.json(result.rows[0]);
+  } catch (err) {
+    console.error('Error updating order:', err);
+    return res.status(500).json({ error: 'Failed to update order' });
   }
 });
 
 // DELETE operation
-app.delete('/fruits/:id', (req, res) => {
-  const index = fruits.findIndex(f => f.id === parseInt(req.params.id));
-  if (index === -1) {
-    res.status(404).send('Fruit not found');
-  } else {
-    fruits.splice(index, 1);
+app.delete('/orders/:id', async (req, res) => {
+  try {
+    const query = {
+      text: 'DELETE FROM orders WHERE id = $1 RETURNING *',
+      values: [req.params.id],
+    };
 
-    res.send('Fruit deleted successfully');
+    const result = await pool.query(query);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: 'Order not found' });
+    }
+
+    return res.json(result.rows[0]);
+  } catch (err) {
+    console.error('Error deleting order:', err);
+    return res.status(500).json({ error: 'Failed to delete order' });
   }
 });
 
